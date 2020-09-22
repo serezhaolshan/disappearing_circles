38906784e5cc1b9fa3b0ad2a4ce9282aeb1f60e9
Serezha <serezhaolshan@gmail.com>
2020-09-22 17:52:57 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("red-circle").onclick = () => {
+    document.getElementById("red-circle").style.display = "none";
+};
+document.getElementById("yellow-circle").onclick = () => {
+    document.getElementById("yellow-circle").style.display = "none";
+};
+document.getElementById("blue-circle").onclick = () => {
+    document.getElementById("blue-circle").style.display = "none";
+};
